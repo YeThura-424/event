c3cfa2eabbff6957a2fdf9096c28543c1412c8a6
YeThura-424 <rathanpoudle.26@gmail.com>
2021-02-28 20:30:58 +0630

    style update

--- custom.js
+++ custom.js
@@ -1,4 +1,15 @@
-$(window).scroll(function(){
-	$('nav').toggleClass('scrolled',
-		$(this).scrollTop()>50);
-})
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
+});
+
+$('a[href*="#"]').on("click", function (e) {
+  e.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top,
+    },
+    500,
+    "linear"
+  );
+});
